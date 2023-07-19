a50128e0414d257714dc567d4c9003338681ecfb
JayG-5 <jg55552758@gmail.com>
2023-07-19 13:09:03 +0900

    feat: post delete

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import Index,DetailView,WriteView,UserView,EditProfileView
+from .views import Index,DetailView,WriteView,UserView,EditProfileView,PostDelete,UpdateView
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
     path("detail/<int:pk>/", DetailView.as_view(), name='detail'),
+    path("detail/<int:pk>/edit", UpdateView.as_view(), name='update'),
+    path("delete/<int:pk>/", PostDelete.as_view(), name='delete'),
     path("write/", WriteView.as_view(), name='write'),
     path("@<str:nickname>/", UserView.as_view(), name='user'),
     path("@<str:nickname>/edit/", EditProfileView.as_view(), name='edit'),
