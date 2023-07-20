62f006dabe262816b34b1e3c10b0c023c2dc7d24
JayG-5 <jg55552758@gmail.com>
2023-07-20 13:09:19 +0900

    Remove files based on .gitignore

--- app/views.py
+++
@@ -1,8 +0,0 @@
-
-from django.views import View
-from django.shortcuts import render, redirect
-
-class MainPageView(View):
-    
-    def get(self,request):
-        return render(request,'index.html')
